8b917bcd9b0c9e20d7ad8b60874bfe17abbdbcd4
Anthony Kim <anthony092777@gmail.com>
2021-06-09 00:38:20 -0400

    add library scrollview

---
+++ Spotify/Controllers/Other/Library/LibraryAlbumsViewController.swift
@@ -0,0 +1,23 @@
+//
+//  LibraryAlbumsViewController.swift
+//  Spotify
+//
+//  Created by Anthony Kim on 6/8/21.
+//
+
+import UIKit
+
+class LibraryAlbumsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+        
+    }
+    
+    
+    
+
+    
+
+}
---
+++ Spotify/Controllers/Other/Library/LibraryPlaylistsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LibraryPlaylistsViewController.swift
+//  Spotify
+//
+//  Created by Anthony Kim on 6/8/21.
+//
+
+import UIKit
+
+class LibraryPlaylistsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+    
+
+   
+}
